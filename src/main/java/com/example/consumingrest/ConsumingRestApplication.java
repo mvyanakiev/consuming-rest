e6c73c08eb961e4c58abd0a4e51d4ad11eb22f75
Milko Yanakiev <myanakiev@hotmail.com>
2020-10-25 18:01:01 +0200

    Initial

---
+++ src/main/java/com/example/consumingrest/ConsumingRestApplication.java
@@ -0,0 +1,42 @@
+package com.example.consumingrest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+public class ConsumingRestApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumingRestApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+
+            for (int i = 0; i < 10; i++) {
+                Quote quote = restTemplate.getForObject(
+                        "https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+                log.info(quote.toString());
+
+                TimeUnit.SECONDS.sleep(1);
+            }
+        };
+    }
+
+}
